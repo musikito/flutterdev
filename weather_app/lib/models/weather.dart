b32a167e37af9b51c4094a6ef88a603017344c9f
JoseMiguel Marte <josemiguelmarte@JoseMiguels-MacBook-Pro.local>
2020-10-11 06:28:41 -0400

    started the weather cond

---
+++ weather_app/lib/models/weather.dart
@@ -0,0 +1,16 @@
+import 'package:weather_app/utils/strings.dart';
+
+enum WeatherCondition {
+  thunderstorm,
+  drizzle,
+  rain,
+  snow,
+  atmosphere,
+  mist,
+  fog,
+  lightcloud,
+  heavycloud,
+  clear,
+  unknown
+} //end of WeatherCondition
+
