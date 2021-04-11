7ad23e5714dfbcdc5edb4a4df15a512a27b2cb2b
JoseMiguel Marte <tumusicalatina@gmail.com>
2021-04-11 13:25:55 -0400

    Dicee finished

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+
 
 void main() {
   return runApp(
@@ -15,9 +18,48 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftSideDice = 1;
+  int rightSideDice = 1;
+
+  void changeDiceFaces(){
+    setState(() {
+      leftSideDice = Random().nextInt(6)+1;
+      rightSideDice = Random().nextInt(6)+1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+               changeDiceFaces();
+              },
+              child: Image.asset('images/dice$leftSideDice.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDiceFaces();
+              },
+              child: Image.asset('images/dice$rightSideDice.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
+
